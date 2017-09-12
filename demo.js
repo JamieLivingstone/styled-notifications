bcb626416f1e09776ff9c4d98b26a340d3c090f6
Jamie Livingstone <jamie.livingstone@outlook.com>
2017-09-12 16:51:57 +0100

    Add eslint

--- demo.js
+++ demo.js
@@ -1,11 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // Success notification
-    const successNotification = createNotification({
-        hideDuration: 4000,
-        onclick: (e) => alert(e),
-        closeButton: true
-    });
+	// Success notification
+	const successNotification = window.createNotification({
+		hideDuration: 4000,
+		newestOnTop: true,
+		onclick: (e) => alert(e),
+		closeButton: true
+	});
 
-    const successButton = document.querySelector('#successNotification');
-    successButton.addEventListener('click', () => successNotification('Success notification...'));
+	const successButton = document.querySelector('#successNotification');
+	successButton.addEventListener('click', () => successNotification('Success notification...'));
 });
--- src/index.js
+++ src/index.js
@@ -1,79 +1,77 @@
 'use strict';
 
 (function Notifications(window) {
-    // Default notification options
-    const defaultOptions = {
-        closeButton: false,
-        newestOnTop: true,
-        positionClass: "notification-bottom-right",
-        onclick: null,
-        showDuration: 300,
-        hideDuration: 1000
-    };
+	// Default notification options
+	const defaultOptions = {
+		closeButton: false,
+		positionClass: 'notification-bottom-right',
+		onclick: null,
+		showDuration: 300,
+		hideDuration: 1000
+	};
 
-    function validateOptions(options) {
-        // Create a copy of options
-        options = Object.assign({}, options);
+	function validateOptions(options) {
+		// Create a copy of options
+		options = Object.assign({}, options);
 
-        // Validate position class
-        function validatePositionClass(className) {
-            const validPositions = [
-                'notification-top-left',
-                'notification-top-right',
-                'notification-bottom-left',
-                'notification-bottom-right',
-                'notification-position-override'
-            ];
+		// Validate position class
+		function validatePositionClass(className) {
+			const validPositions = [
+				'notification-top-left',
+				'notification-top-right',
+				'notification-bottom-left',
+				'notification-bottom-right',
+				'notification-position-override'
+			];
 
-            return validPositions.indexOf(className) > -1;
-        }
+			return validPositions.indexOf(className) > -1;
+		}
 
-        // Verify position, if invalid reset to default
-        if (!validatePositionClass(options.positionClass)) {
-            console.warn('An invalid notification position class has been specified.');
-            options.positionClass = defaultOptions.positionClass;
-        }
+		// Verify position, if invalid reset to default
+		if (!validatePositionClass(options.positionClass)) {
+			console.warn('An invalid notification position class has been specified.');
+			options.positionClass = defaultOptions.positionClass;
+		}
 
-        // Verify onClick is a function
-        if (options.onclick && typeof options.onclick !== 'function') {
-            console.warn('Notification on click must be a function.');
-            options.onclick = defaultOptions.onclick;
-        }
+		// Verify onClick is a function
+		if (options.onclick && typeof options.onclick !== 'function') {
+			console.warn('Notification on click must be a function.');
+			options.onclick = defaultOptions.onclick;
+		}
 
-        return options;
-    }
+		return options;
+	}
 
-    // Create a new notification instance
-    function createNotification(options) {
-        options = validateOptions(Object.assign({}, defaultOptions, options));
+	// Create a new notification instance
+	function createNotification(options) {
+		options = validateOptions(Object.assign({}, defaultOptions, options));
 
-        // Return a notification function
-        return function notification(message) {
-            const notificationEl = document.createElement('div');
+		// Return a notification function
+		return function notification(message) {
+			const notificationEl = document.createElement('div');
 
-            // Display close button
-            if(options.closeButton) {
-                const close = document.createElement('div');
-                const closeButton = document.createElement('button');
-                closeButton.innerText = 'X';
-                close.append(closeButton);
-                closeButton.addEventListener('click', () => document.body.removeChild(notificationEl));
-                notificationEl.append(close);
-            }
+			// Display close button
+			if(options.closeButton) {
+				const close = document.createElement('div');
+				const closeButton = document.createElement('button');
+				closeButton.innerText = 'X';
+				close.append(closeButton);
+				closeButton.addEventListener('click', () => document.body.removeChild(notificationEl));
+				notificationEl.append(close);
+			}
 
-            // Append message
-            notificationEl.append(document.createElement('p').innerText = message);
+			// Append message
+			notificationEl.append(document.createElement('p').innerText = message);
 
-            // Append to DOM
-            document.body.appendChild(notificationEl);
-        }
-    }
+			// Append to DOM
+			document.body.appendChild(notificationEl);
+		};
+	}
 
-    // Add Notifications to window to make globally accessible
-    if (window.createNotification) {
-        console.warn('Window already contains a create notification function. Have you included the script twice?');
-    } else {
-        window.createNotification = createNotification;
-    }
-
+	// Add Notifications to window to make globally accessible
+	if (window.createNotification) {
+		console.warn('Window already contains a create notification function. Have you included the script twice?');
+	} else {
+		window.createNotification = createNotification;
+	}
 })(window);
