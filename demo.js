a08808f47e60af9cbf4ee923740fca14938c47d3
Jamie Livingstone <jamie.livingstone@outlook.com>
2017-09-12 17:11:53 +0100

    Simple demo page

--- demo.js
+++ demo.js
@@ -1,12 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
+	// Generate a dummy message
+	function generateMessage(messagesArray = ['Logged in successfully!', 'Saved data', 'Uploading video with some dummy text', 'Congratulations', 'Failed to upload']) {
+		const index = Math.round(Math.random() * (messagesArray.length - 1));
+		return messagesArray[index];
+	}
+
 	// Success notification
 	const successNotification = window.createNotification({
 		hideDuration: 4000,
 		newestOnTop: true,
 		onclick: (e) => alert(e),
-		closeButton: true
+		closeButton: true,
+		className: 'success'
 	});
 
 	const successButton = document.querySelector('#successNotification');
-	successButton.addEventListener('click', () => successNotification('Success notification...'));
+	successButton.addEventListener('click', () => successNotification(generateMessage()));
 });
