ad0fc524180a5bfa24a69a257f19a98ec9d02f8e
Jamie Livingstone <jamie.livingstone@outlook.com>
2017-09-14 17:03:10 +0100

    Demonstration page

--- demo/demo.js
+++ demo/demo.js
@@ -2,18 +2,33 @@
 
 // Written using ES5 JS for browser support
 window.addEventListener('DOMContentLoaded', function () {
-	// Success notification example
-	var successNotification = window.createNotification({
-		closeOnClick: true,
-		showDuration: 300000,
-		displayCloseButton: true,
-		theme: 'info',
-		positionClass: 'nfc-top-left',
-	});
+	var form = document.querySelector('form');
 
-	var successButton = document.querySelector('#successNotification');
+	form.addEventListener('submit', function (e) {
+		e.preventDefault();
 
-	successButton.addEventListener('click', function () {
-		successNotification({ title: 'Success', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus tellus dui, placerat vitae sagittis ac, imperdiet in lorem. Integer fringilla malesuada condimentum. Donec lacus nibh, dictum nec neque a, dignissim sollicitudin tortor. Donec ut mi nisi. Curabitur egestas blandit vulputate. Pellentesque id metus mollis, cursus lectus vitae, pharetra est. Donec tortor urna, condimentum sit amet diam venenatis, rutrum tempus eros. Donec pretium orci in scelerisque aliquam. Sed pharetra ultrices iaculis. Vestibulum et consectetur neque. Praesent auctor nulla sed massa euismod consectetur. Vivamus scelerisque scelerisque lacus, nec aliquam mauris rhoncus nec. Ut a elit viverra, mollis metus ac, pharetra diam.'});
+		// Form elements
+		var title = form.querySelector('#title').value;
+		var message = form.querySelector('#message').value;
+		var position = form.querySelector('#position').value;
+		var duration = form.querySelector('#duration').value;
+		var theme = form.querySelector('#theme').value;
+		var closeOnClick = form.querySelector('#close').checked;
+		var displayClose = form.querySelector('#closeButton').checked;
+
+		if(!message) {
+			message = 'You did not enter a message...';
+		}
+
+		window.createNotification({
+			closeOnClick: closeOnClick,
+			displayCloseButton: displayClose,
+			positionClass: position,
+			showDuration: duration,
+			theme: theme
+		})({
+			title: title,
+			message: message
+		});
 	});
 });
